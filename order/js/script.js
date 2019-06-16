28b9f388987c2d78be418ac438a87703e158a3e4
markmuba <markmumba01@gmail.com>
2019-06-16 20:23:54 +0300

    js

--- order/js/script.js
+++ order/js/script.js
@@ -1,11 +1,34 @@
-var top1={ name:"Pepperoni",price:600}
-var top2={name:"Mushroom",price:500}
-var top3={name:"Bacon",price:700}
-var top4={name:"Extra Cheese",price:200}
-var top5={name: "Pesto",price:600}
+var crustOne={name:"crispy",price:100}
+var crustTwo={name:"stuffed",price:200}
+var crustThree={name:"Glutten Free",price:50}
+
+var Crust={
+    name:"crusts",
+    types:[crustOne,crustTwo,crustThree]
+}
 
 
 
+var topOne={ name:"Pepperoni",price:600,crust}
+var topTwo={name:"Mushroom",price:500,crust}
+var topThree={name:"Bacon",price:700,crust}
+var topFour={name:"Extra Cheese",price:200,crust}
+var topFive={name: "Pesto",price:600,crust}
+
+var ConstantOne={
+    name:"toppings",
+    types:[topOne,topTwo,topThree,topFour,topFive,]
+}
+
+var sizeOne={name:"small",price:1000,ConstantOne}
+var sizeTwo={name:"medium",price:1200,ConstantOne}
+var sizeThree={name:"large",price:1500,ConstantOne}
+  
+var sizes=[sizeOne,sizeTwo,sizeThree]
+ 
+/*sizes.forEach(function(size){
+    alert(size.ConstantOne.crust)
+})*/
 
 
 
