231584fbe9ab11aec4671a6d0267b4edcff73a7c
markmuba <markmumba01@gmail.com>
2019-06-17 11:19:05 +0300

    js

---
+++ README.md
@@ -0,0 +1,4 @@
+# mark's pizza
+![web page](./image/1.png)
+### description 
+This is website where one can order pizza
--- order/js/script.js
+++ order/js/script.js
@@ -40,30 +40,75 @@ var Crust={
 
 
 
-var topOne={ name:"Pepperoni",price:600,crust}
-var topTwo={name:"Mushroom",price:500,crust}
-var topThree={name:"Bacon",price:700,crust}
-var topFour={name:"Extra Cheese",price:200,crust}
-var topFive={name: "Pesto",price:600,crust}
+var topOne={ name:"Pepperoni",price:600,Crust}
+var topTwo={name:"Mushroom",price:500,Crust}
+var topThree={name:"Bacon",price:700,Crust}
+var topFour={name:"Extra Cheese",price:200,Crust}
+var topFive={name: "Pesto",price:600,Crust}
 
-var ConstantOne={
+var Toppings={
     name:"toppings",
     types:[topOne,topTwo,topThree,topFour,topFive,]
 }
 
-var sizeOne={name:"small",price:1000,ConstantOne}
-var sizeTwo={name:"medium",price:1200,ConstantOne}
-var sizeThree={name:"large",price:1500,ConstantOne}
+var sizeOne={name:"small",price:1000,Toppings}
+var sizeTwo={name:"medium",price:1200,Toppings}
+var sizeThree={name:"large",price:1500,Toppings}
   
 var sizes=[sizeOne,sizeTwo,sizeThree]
  
 /*sizes.forEach(function(size){
-    alert(size.ConstantOne.crust)
+    alert(size.Toppings.crust)
 })*/
 
 
 
+/*******************************Bussiness logic***************************** */
+ function order(size,topping,crust){
+    
+    // var sideA =parseInt(document.getElementById("sideA").value);
+    sizes=["large","medium","small"];
+    Toppings=["Pepperoni","Mushroom","Bacon","Extra Cheese","Pesto"];
+    Crust=["Crispy","Stuffed","Glutten Free"];
+    price =0;
+    var results=document.querySelector("#final");
 
+    if (size ===sizes[0]){
+         results.innerHTML= price+=1500
+    }
+    if (size===sizes[1]){
+      results.innerHTML= price+=1200
+    }
+    if (size===sizes[2]){
+      results.innerHTML= price+=1000
+    }
+    if (topping===Toppings[0]){
+        price+=600
+    }
+    if (topping===Toppings[1]){
+     price+=500
+    } 
+    if(topping===Toppings[2]){
+        price+=700
+    }  
+    if(topping===Toppings[3]){
+        price+=200
+    }
+    if(topping===Toppings[4]){
+        price+=600
+    }
+    if (crust===Crust[0]){
+        price+=100
+    }
+    if(crust===crust[1]){
+        price+=200
+    }
+    if (crust===Crust[2]){
+        price+=50
+    }
+
+}
+  
 
 
 
